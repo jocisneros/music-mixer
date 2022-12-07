72fb44b0cc2fbeb5e69c38a15f49bd9f02b2d380
Jose Cisneros <33441935+jocisneros@users.noreply.github.com>
2022-12-06 23:58:04 -0800

    updating functions.

--- netlify/functions/login.ts
+++ netlify/functions/login.ts
@@ -1,24 +1,35 @@
 // login.ts
 
 import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+import axios, { AxiosResponse } from 'axios';
 
 type LoginData = {
     authorizationCode: string,
 }
 
+type AccessTokenResponse = AxiosResponse<{
+    access_token: string,
+    token_type: string,
+    scope: string,
+    expires_in: number,
+    refresh_token: string,
+}>
+
 const AUTH_HEADER = 'Basic ' + Buffer.from(
     `${process.env.VITE_SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
 ).toString('base64');
 
+// @ts-ignore
 export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     const { authorizationCode } = (JSON.parse(event.body || '') as LoginData)
 
-    return fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
+    return axios.post<any, AccessTokenResponse>('https://accounts.spotify.com/api/token', {
+        'grant_type': 'authorization_code',
+        'refresh_token': authorizationCode,
+        'redirect_uri': process.env.VITE_SPOTIFY_REDIRECT_URI
+    }, { headers: {
             'Authorization': AUTH_HEADER,
             'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${process.env.VITE_SPOTIFY_REDIRECT_URI}`
-    }).then(response => response.json());
+        }
+    });
 }
--- netlify/functions/refresh.ts
+++ netlify/functions/refresh.ts
@@ -1,24 +1,33 @@
 // refresh.ts
 
 import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+import axios, { AxiosHeaders, AxiosResponse } from 'axios';
 
 type RefreshData = {
     refreshToken: string,
 }
 
+type RefreshedTokenResponse = AxiosResponse<{
+    access_token: string,
+    token_type: string,
+    scope: string,
+    expires_in: number,
+}>
+
 const AUTH_HEADER = 'Basic ' + Buffer.from(
     `${process.env.VITE_SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
 ).toString('base64');
 
+// @ts-ignore
 export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     const { refreshToken } = (JSON.parse(event.body || '') as RefreshData)
 
-    return fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
+    return axios.post<any, RefreshedTokenResponse>('https://accounts.spotify.com/api/token', {
+        'grant_type': 'refresh_token',
+        'refresh_token': refreshToken
+    }, { headers: {
             'Authorization': AUTH_HEADER,
             'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `grant_type=refresh_token&refresh_token=${refreshToken}`
-    }).then(response => response.json());
+        }
+    });
 }
