82c5fcbfeb4c9beff6ef5fb644b013f03bc61a12
Jose Cisneros <33441935+jocisneros@users.noreply.github.com>
2022-12-07 01:10:09 -0800

    revamping login function.

--- netlify/functions/login.ts
+++ netlify/functions/login.ts
@@ -11,24 +11,28 @@ const AUTH_HEADER = 'Basic ' + Buffer.from(
     `${process.env.VITE_SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
 ).toString('base64');
 
-// @ts-ignore
 export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-    const { authorizationCode } = (JSON.parse(event.body || '') as LoginData);
+    try {
+        const { authorizationCode } = (JSON.parse(event.body || '') as LoginData);
 
-    const body = `grant_type=authorization_code&code=${authorizationCode}` +
-                 `&redirect_uri=${process.env.VITE_SPOTIFY_REDIRECT_URI}`;
+        const body = `grant_type=authorization_code&code=${authorizationCode}` +
+                     `&redirect_uri=${process.env.VITE_SPOTIFY_REDIRECT_URI}`;
 
-    return axios.post('https://accounts.spotify.com/api/token', body, {
-        headers: {
-            'Authorization': AUTH_HEADER,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(
-        response => {
-            console.log(response)
-            return ({ statusCode: 200, body: JSON.stringify(response.data) })
-        }
-    ).catch(
-        error => ({ statusCode: 422, body: `Error ${error}`})
-    );
+        const response = await axios.post('https://accounts.spotify.com/api/token', body, {
+            headers: {
+                'Authorization': AUTH_HEADER,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 422,
+            body: `Error ${error}`
+        };
+    }
 }
